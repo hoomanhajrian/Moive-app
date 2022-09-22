efd3b38cf53970473f578675c70d3ea5388f84f2
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2022-09-22 12:39:55 -0700

    added expand feature for each movie

--- src/components/tabs/subComponents/MovieCard.js
+++ src/components/tabs/subComponents/MovieCard.js
@@ -3,30 +3,59 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import { CardActionArea, Typography, Box } from '@material-ui/core';
+import { CardActionArea, Typography, Box, CardContent, Collapse } from '@material-ui/core';
 import NoAdultContentIcon from '@mui/icons-material/NoAdultContent';
 import { useHistory } from 'react-router-dom';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import CardActions from '@mui/material/CardActions';
+
+
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
 
 const getStyles = makeStyles(theme => ({
     card: {
         height: '100%',
         backgroundColor: '#1b1b1b',
         color: 'white',
-        padding: '1rem'
+        padding: '1rem',
+        borderRadius: '15px'
     },
     wrapper: {
         padding: '.5rem'
     },
     title: {
-        '&:nextchild': {
-            color: 'white'
-        }
+        color: 'white'
+    },
+    cardActionArea: {
+        padding: '1rem',
+        textAlign: 'center'
     }
 }))
 
 const MovieCard = props => {
     const classes = getStyles();
     const { id, title, release_date, poster_path, backdrop_path, adult, overview, original_language, vote_average, vote_count } = props.data;
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+
+
     const history = useHistory();
 
     const onCardClick = (e) => {
@@ -37,28 +66,40 @@ const MovieCard = props => {
 
 
     return (
-        <Card className={classes.card} >
-            <CardActionArea id={id} onClick={onCardClick}>
-                <CardHeader title={title} subheader={`Release Date: ${release_date}`} className={classes.title} />
+        <Card className={classes.card} variant="outlined">
+            <CardContent>
+                <CardHeader title={title} subheaderTypographyProps={{ color: 'white' }} subheader={`Release Date: ${release_date}`} className={classes.title} />
                 {adult ? <NoAdultContentIcon /> : <></>}
+                <CardMedia
+                    component="img"
+                    height="auto"
+                    image={`https://image.tmdb.org/t/p/original/${backdrop_path}`} alt={title}
+                />
+                <Box className={classes.wrapper}>
+                    <Typography variant="subtitle1" ><strong>Score/Votes: </strong>{vote_average} / {vote_count}</Typography>
+                    <Typography variant="subtitle1" >
+                        <strong>Languages: </strong>{original_language}
+                    </Typography>
+                </Box>
+            </CardContent>
+            <CardActionArea className={classes.cardActionArea} id={id} onClick={onCardClick}>
+                <Typography variant='h4'>Click here for trailer</Typography>
             </CardActionArea>
-            <CardMedia
-
-                component="img"
-                height="auto"
-                image={`https://image.tmdb.org/t/p/original/${backdrop_path}`} alt={title}
-            />
-            <Box className={classes.wrapper}>
-                <Typography variant="subtitle1" ><strong>Score/Votes: </strong>{vote_average} / {vote_count}</Typography>
-                <Typography variant="subtitle1" >
-                    <strong>Languages: </strong>{original_language}
-                </Typography>
-                <details>
-                    <summary><strong>Description </strong></summary>
-                    <p>{overview}</p>
-                </details>
-
-            </Box>
+            <CardActions disableSpacing>
+                <ExpandMore
+                    expand={expanded}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon color='primary' />
+                </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    {overview}
+                </CardContent>
+            </Collapse>
         </Card>
     )
 }
